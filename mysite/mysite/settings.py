badc6ff13c432a74a88e21a0bf91840b34d1968f
VISION <rama.re.1128@gmail.com>
2023-03-04 20:13:01 +0300

    generalConcept + SubConcept + TheoreticalData: edited

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "smurfssaving@gmail.com"
-EMAIL_HOST_PASSWORD = "kahgaldnwbmjsnrr"
+EMAIL_HOST_PASSWORD = ""
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
